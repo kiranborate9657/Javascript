4e4e8d047c0a07edc97ec2b367cffddf2e9694e2
Kiran Borate <boratek844@gmail.com>
2026-02-08 23:08:20 +0530

    Create firstcode.js

---
+++ firstcode.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple JavaScript</title>
+</head>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button onclick="showMessage()">Click Me</button>
+
+<script>
+function showMessage() {
+    alert("Hello! This is my first JavaScript code.");
+}
+</script>
+
+</body>
+</html>
