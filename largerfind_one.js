21fbc295f69e817a23e5c229b4543c69630ac212
Kiran Borate <boratek844@gmail.com>
2026-02-12 21:41:07 +0530

    Add script to find the largest of two numbers

---
+++ largerfind_one.js
@@ -0,0 +1,8 @@
+let a = parseInt(prompt("Enter first number:"));
+let b = parseInt(prompt("Enter second number:"));
+
+if (a > b) {
+    console.log("Largest number is: " + a);
+} else {
+    console.log("Largest number is: " + b);
+}
