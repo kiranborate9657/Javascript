6d03a90deee5f0df09f7504449682678ed3c15d7
Kiran Borate <boratek844@gmail.com>
2026-02-21 23:09:33 +0530

    Add Variables.js with name and age logging

---
+++ Varibles.js
@@ -0,0 +1,11 @@
+var name = "Kiran";
+var age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+if (age >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Not eligible to vote");
+}
