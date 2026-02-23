a31a19cb1dc2a395d2526b8b3f486d5bcf23d6d7
Kiran Borate <boratek844@gmail.com>
2026-02-23 19:09:17 +0530

    Add pre and post decrement example in JavaScript

---
+++ pre_post_decrement.js
@@ -0,0 +1,8 @@
+var x=100;
+var y=200;
+
+console.log("Before the pre and post decrement");
+console.log("x before pre decrement: "+x);
+console.log("y before post decrement: "+y);
+
+console.log("After the both pre and the post decrement:  "  + (x-- + --y));
