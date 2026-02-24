b909b772ab86be6ec460ae7fa0357a47c9a4fb58
Kiran Borate <boratek844@gmail.com>
2026-02-24 19:12:06 +0530

    Add input.js to check if a number is even or odd

---
+++ input.js
@@ -0,0 +1,9 @@
+let num=prompt("Enter a number : ");
+num=Number(num);
+if(num%2==0){
+    console.log("Even number");
+}
+else{
+    console.log("Odd number");
+}
+console.log("The number is:", num);
