495d44ca12f3e3582db34674fae44ecce52b2fc3
Kiran Borate <boratek844@gmail.com>
2026-02-25 21:52:14 +0530

    Add controlStatementExample function for greetings

---
+++ control_statement.js
@@ -0,0 +1,19 @@
+function controlStatementExample(statement) {
+    if(statement==="gm"){
+        console.log("Good Morning");
+    }
+    else if(statement==="ga"){
+        console.log("Good Afternoon");
+    }
+    else if(statement==="ge"){
+        console.log("Good Evening");
+    }
+    else{
+        console.log("Invalid Statement");
+    }
+
+}
+controlStatementExample("gm");
+controlStatementExample("ga");
+controlStatementExample("ge");
+controlStatementExample("gn");
