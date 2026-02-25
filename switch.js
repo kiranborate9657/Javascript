91de4e77bd7930f0bc41cbef93fe4011d691dc29
Kiran Borate <boratek844@gmail.com>
2026-02-25 21:53:35 +0530

    Add switch statement to check number value

---
+++ switch.js
@@ -0,0 +1,21 @@
+var number=5;
+switch(number){
+    case 1:
+        console.log("number is 1");
+        break;
+    case 2:
+        console.log("number is 2");
+        break;
+    case 3:
+        console.log("number is 3");
+        break;
+    case 4:
+        console.log("number is 4");
+        break;
+    case 5:
+        console.log("number is 5");
+        break;
+    default:
+        console.log("number is not between 1 and 5");
+}
+   
