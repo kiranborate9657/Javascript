6619705df5e6a9a2178c28a992ad8ae3324c5885
Kiran Borate <boratek844@gmail.com>
2026-02-27 22:08:46 +0530

    Create for1.js

---
+++ for1.js
@@ -0,0 +1,12 @@
+function printNumbers() {
+    for(var i=0;i<=10;i++){
+        console.log(i);
+    }
+
+    //print the number in the reverse order
+for(var j=10;j>=0;j--){
+    console.log(j);
+}
+}
+printNumbers();
+
