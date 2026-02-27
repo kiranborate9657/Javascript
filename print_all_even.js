118a494d5512c440c9257040616a502b65d095a6
Kiran Borate <boratek844@gmail.com>
2026-02-27 22:08:14 +0530

    Add function to print all even numbers from 0 to 100

---
+++ print_all_even.js
@@ -0,0 +1,17 @@
+function even(){
+    for(var i=0;i<=100;i=i+2){
+        console.log(i);
+    }
+    var j=100;
+    if(j%2==0){
+        console.log(j);
+        j--;
+
+    }
+
+
+
+}
+even();
+
+
