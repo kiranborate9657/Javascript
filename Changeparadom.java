d159ea25bc9c433ba3dbf315113dbecda92a4e81
Kiran Borate <boratek844@gmail.com>
2026-03-23 21:09:50 +0530

    Create Changeparadom.java

---
+++ Changeparadom.java
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DOM Example 1</title>
+</head>
+<body>
+
+<p id="text">Hello Kiran!</p>
+
+<button onclick="changeText()">Click Me</button>
+
+<script src="script.js"></script>
+
+</body>
+</html>
