eba459653d3947a69325f3bb2a1a0daf04632b31
Kiran Borate <boratek844@gmail.com>
2026-03-23 21:11:06 +0530

    Create Hidebutten.js

---
+++ Hidebutten.js
@@ -0,0 +1,11 @@
+function toggleText() {
+
+    var p = document.getElementById("para");
+
+    if (p.style.display === "none") {
+        p.style.display = "block";
+    } 
+    else {
+        p.style.display = "none";
+    }
+}
