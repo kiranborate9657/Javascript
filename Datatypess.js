73a2d30c953d3a482902473e1d8953ab2966bd5c
Kiran Borate <boratek844@gmail.com>
2026-04-09 23:33:30 +0530

    Add Datatypess.js to demonstrate JavaScript types

---
+++ Datatypess.js
@@ -0,0 +1,11 @@
+let number = 10;           // Number
+let text = "Hello";        // String
+let isStudent = true;      // Boolean
+let value = null;          // Null
+let data;                  // Undefined
+
+console.log(typeof number);
+console.log(typeof text);
+console.log(typeof isStudent);
+console.log(typeof value);
+console.log(typeof data);
