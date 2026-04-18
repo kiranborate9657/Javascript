a680c791d5aaa73e40be346170b03bed6e761d89
Kiran Borate <boratek844@gmail.com>
2026-04-18 23:06:57 +0530

    Add HTML page to calculate factorial of a number

---
+++ Factorial.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+
+let num = 5;
+let fact = 1;
+
+for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+}
+
+document.write("Factorial = " + fact);
+
+</script>
+
+</body>
+</html>
