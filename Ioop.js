a2010a30a159338cb9fc61da65922acda5ae4398
Kiran Borate <boratek844@gmail.com>
2026-04-27 23:57:05 +0530

    Add HTML loop example in Ioop.js

---
+++ Ioop.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Loop Example</title>
+</head>
+<body>
+
+<script>
+    for(let i = 1; i <= 10; i++)
+    {
+        document.write(i + "<br>");
+    }
+</script>
+
+</body>
+</html>
